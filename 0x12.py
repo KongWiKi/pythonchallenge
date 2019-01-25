79d4c0fd32cf42000c33fee74f139c08a1634513
KongWiki <kongwiki@163.com>
2019-01-25 23:15:35 +0800

    更新到第12题

--- 0x12.py
+++ 0x12.py
@@ -3,4 +3,10 @@
 @file:   0x12.py
 @time:   19-1-25下午11:07
 @contact: kongwiki@163.com
-""" 
+"""
+data = open("./data/0x12/evil2.gfx", "rb").read()
+print(len(data))
+
+for i in range(5):
+    open('./data/0x12/%d.jpg'%i, 'wb').write(data[i::5])
+
